b3013dc7a5c08594d6995b1ae06c1ffe7ed4ca58
xuedishangdemeigui <2216336843@qq.com>
2024-05-18 03:24:33 +0800

    feat(wip): devloping home page and add login page

--- explore-fe/src/app/page.tsx
+++ explore-fe/src/app/page.tsx
@@ -1,15 +1,36 @@
 "use client"
 
-import { Box, Input, InputGroup, InputLeftElement, Button, Flex, Spacer, Heading, Link, Divider, VStack, Card, CardBody, CardFooter, Grid, Text } from "@chakra-ui/react";
+import {
+  Box, Input, InputGroup, InputLeftElement, Button, Flex,
+  Spacer, Heading, Link, Divider, VStack, Card, CardBody, CardFooter, Grid, Text,
+  Container
+} from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+
+interface TopicIF {
+  title: string,
+  username: string,
+  time: string,
+}
+
+const Topic = (props: TopicIF) => {
+  return (
+    <Card size='sm' w='800px'>
+      <CardBody>
+        <Link>{props.title}</Link>
+        <Text fontSize='xs'>by {props.username} at {props.time}</Text>
+      </CardBody>
+    </Card>
+  )
+}
 
 export default function Home() {
   return (
     <Grid minHeight='100%' templateRows={'60px auto 120px'}>
       <Box>
-        <Flex alignItems='center' gap='8' padding={'5px 150px'}>
+        <Flex alignItems='center' gap='8' padding={'5px 150px'} bg='white'>
           <Box p='2'>
-            <Heading size='md'><Link>Explore</Link></Heading>
+            <Heading size='md'><Link href='/'>Explore</Link></Heading>
           </Box>
           <Spacer />
           <InputGroup w={250} size='sm'>
@@ -19,33 +40,26 @@ export default function Home() {
             <Input type='tel' placeholder='Search Explore' />
           </InputGroup>
           <Link>About Explore</Link>
-          <Button size='md' variant='ghost'>Login</Button>
+          <Link href='/user/login'>Login</Link>
         </Flex>
         <Divider />
       </Box>
       <Flex justifyContent='center'>
         <VStack>
-          <Card size='sm' w='800px'>
-            <CardBody>This is the title of a topic</CardBody>
-            <CardFooter>Hello world!</CardFooter>
-          </Card>
-          <Card size='sm' w='800px'>
-            <CardBody>This is the title of a topic</CardBody>
-            <CardFooter>Hello world!</CardFooter>
-          </Card>
-          <Card size='sm' w='800px'>
-            <CardBody>This is the title of a topic</CardBody>
-            <CardFooter>Hello world!</CardFooter>
-          </Card>
+          <Topic title='This is the title of a topic' username='ZhangChi' time='2024-05-20 19:41:33 (UTC+8)' />
+          <Topic title='This is the title of a topic' username='ZhangChi' time='2024-05-20 19:41:33 (UTC+8)' />
+          <Topic title='This is the title of a topic' username='ZhangChi' time='2024-05-20 19:41:33 (UTC+8)' />
+          <Topic title='This is the title of a topic' username='ZhangChi' time='2024-05-20 19:41:33 (UTC+8)' />
+          <Topic title='This is the title of a topic' username='ZhangChi' time='2024-05-20 19:41:33 (UTC+8)' />
         </VStack>
       </Flex>
       <Box>
-        <Divider />
-        <Flex justifyContent='center' >
+        <Divider color='gray' colorScheme='gray' />
+        <Flex justifyContent='center' bg='white'>
           <VStack w='800px'>
             <Flex justifyContent='left' alignItems='center' w='100%' gap={4} padding={'5px 0 0 2px'}>
-              <Link color='teal.500' as='b'>About</Link>
-              <Link color='teal.500' as='b'>Help</Link>
+              <Link color='gray.500' as='b'>About</Link>
+              <Link color='gray.500' as='b'>Help</Link>
             </Flex>
             <Box w='100%' padding={'2px 0 2px 0'}>
               <Text fontSize='sm'>Hi! Welcome to Explore.</Text>
---
+++ explore-fe/src/app/user/login/page.tsx
@@ -0,0 +1,173 @@
+/* 
+Reference: 
+  1. https://react-hook-form.com/get-started
+  2. https://github.com/projectsbydan/nextjs-react-form-chakra-ui/blob/master/components/forms/registration-form.tsx
+  3. https://v2.chakra-ui.com/getting-started/with-hook-form
+*/
+
+'use client'
+
+import {
+  VStack,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  Icon,
+  Box,
+  Square,
+  HStack,
+  Alert,
+  AlertIcon,
+  useDisclosure,
+  CloseButton,
+} from '@chakra-ui/react'
+
+import { EmailIcon, LockIcon } from '@chakra-ui/icons'
+import { RiEyeFill, RiEyeOffFill } from "react-icons/ri";
+import { FaHandsClapping } from "react-icons/fa6";
+
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { SingleResponseBodyIF } from '@/app/model/base';
+import { useRouter } from 'next/navigation';
+
+interface IRegistration {
+  name: string;
+  email: string;
+  pwd: string;
+}
+
+interface TipIF {
+  status: 'success' | 'error' | undefined,
+  msg: string | null,
+}
+
+export default function Register() {
+  const {
+    handleSubmit, // handels the form submit event
+    register, // ties the inputs to react-form
+    formState: { errors, isSubmitting }, // gets errors and "loading" state
+  } = useForm<IRegistration>();
+
+  const {
+    isOpen: isVisible,
+    onClose,
+    onOpen,
+  } = useDisclosure({ defaultIsOpen: false });
+
+  const [show, setShow] = useState<boolean>(false);
+  const handleClick = () => setShow(!show);
+
+  const [tip, setTip] = useState<TipIF>({ status: undefined, msg: null });
+
+  const router = useRouter();
+
+  const onRegistered = async (data: IRegistration) => {
+    const resp = await fetch('/api/user/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+
+    if (!resp.ok) {
+      setTip({ status: 'error', msg: 'Request failed' });
+      onOpen();
+    } else {
+      const body: SingleResponseBodyIF = await resp.json();
+      if (body.code !== 0) {
+        setTip({ status: 'error', msg: body.msg });
+        onOpen();
+      } else {
+        setTip({ status: 'success', msg: 'Registered successfully' });
+        onOpen();
+        router.push('/user/welcome')
+      }
+    }
+  }
+
+  return (
+    <Box className="flex min-h-screen flex-col items-center justify-between p-24" bgGradient='linear(to-r, green.200, pink.500)'>
+      <Card w='420px' variant='elevated' bg='gray.50'>
+        <CardHeader>
+          <HStack>
+            <Heading size='md'>Welcome to Explore</Heading>
+            <Square><Icon as={FaHandsClapping} color='orange' w={5} h={5} /></Square>
+          </HStack>
+        </CardHeader>
+        <form onSubmit={handleSubmit(onRegistered)} noValidate>
+          <CardBody>
+            <VStack>
+              {/*
+                Email input
+              */}
+              <FormControl isRequired isInvalid={errors.email ? true : undefined}>
+                <FormLabel htmlFor='email'>Email</FormLabel>
+                <InputGroup>
+                  <InputLeftElement pointerEvents='none'>
+                    <EmailIcon color='gray.600' />
+                  </InputLeftElement>
+                  <Input id='email' type='email' {...register("email", {
+                    required: "Email is required.",
+                    pattern: { value: /[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}/, message: "Email is invalid." },
+                  })} />
+                </InputGroup>
+                <FormErrorMessage>{errors.email && errors.email.message}</FormErrorMessage>
+              </FormControl>
+              {/*
+                Password input
+              */}
+              <FormControl isRequired isInvalid={errors.pwd ? true : undefined}>
+                <FormLabel htmlFor='pwd'>Password</FormLabel>
+                <InputGroup size='md'>
+                  <InputLeftElement pointerEvents='none'>
+                    <LockIcon color='gray.600' />
+                  </InputLeftElement>
+                  <Input id='pwd' type={show ? 'text' : 'password'} {...register("pwd", {
+                    required: "Password is required.",
+                    pattern: {
+                      value: /^(\S)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_])[a-zA-Z0-9~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_]{10,16}$/,
+                      message: "The rule of setting password:\n1. Password must not contain any whitespace.\n2. Password must contain uppercases, lowercases, numeric characters and special characters (~`!@#$%^&*()--+={}[]|\:;\"'<>,.?/_).\n3. Password's length must between 10 to 16.",
+                    },
+                  })} />
+                  <InputRightElement width='4.5rem'>
+                    <Button h='1.75rem' size='sm' onClick={handleClick}>
+                      {show ? < Icon as={RiEyeFill} color='gray.600' /> : < Icon as={RiEyeOffFill} color='gray.600' />}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                <FormHelperText></FormHelperText>
+                <FormErrorMessage whiteSpace='pre-wrap' wordBreak='break-word'>{errors.pwd && errors.pwd.message}</FormErrorMessage>
+              </FormControl>
+            </VStack>
+          </CardBody>
+          <CardFooter>
+            <VStack width='100%'>
+              {/* Tip */}
+              {isVisible ?
+                <Alert status={tip.status}>
+                  <AlertIcon />
+                  <Box fontSize='sm'>{tip.msg}</Box>
+                  <CloseButton marginLeft='auto' onClick={onClose} />
+                </Alert> : <></>}
+              {/* Submit button */}
+              <Button type='submit' colorScheme='teal' variant='solid' size='md' width='100%' isLoading={isSubmitting}>
+                Login
+              </Button>
+            </VStack>
+          </CardFooter>
+        </form>
+      </Card>
+    </Box >
+  );
+}
