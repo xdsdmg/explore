c4472365194640bad77605fcce44e326f7be2985
xuedishangdemeigui <2216336843@qq.com>
2024-06-07 00:30:56 +0800

    feat(wip): devloping home page

--- explore-fe/src/app/components/pagination.tsx
+++ explore-fe/src/app/components/pagination.tsx
@@ -1,9 +1,9 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import { Button, HStack, IconButton } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const defaultPageSize = 20;
-const defaultBtnNum = 5;
+const defaultBtnNum = 3;
 
 interface PageNumBtnProps {
   setPos: any,
@@ -13,42 +13,62 @@ interface PageNumBtnProps {
   pageTotal: number,
 }
 
+interface NumElement {
+  readonly type: 'num',
+  num: number,
+}
+
+interface EllipsisElement {
+  readonly type: 'ellipsis',
+  direction: boolean,
+}
+
+function getElements(pos: number, pageTotal: number): (NumElement | EllipsisElement)[] {
+  let elements: (NumElement | EllipsisElement)[] = [];
+
+  let gap: number = (defaultBtnNum - 1) / 2;
+
+  let begin = pos - gap;
+  let end = pos + gap;
+
+  if (begin < 1) {
+    begin = 1;
+    end = begin + 2 * gap <= pageTotal ? begin + 2 * gap : pageTotal;
+  }
+  if (end > pageTotal) {
+    end = pageTotal;
+    begin = end - 2 * gap >= 1 ? end - 2 * gap : 1;
+  }
+
+  if (begin > 1) {
+    elements.push({ type: 'num', num: 1 });
+    if (begin > 3) {
+      elements.push({ type: 'ellipsis', direction: false });
+    } else if (begin == 3) {
+      elements.push({ type: 'num', num: 2 });
+    }
+  }
+
+  for (let i = begin; i <= end; i++) {
+    elements.push({ type: 'num', num: i });
+  }
+
+  if (end < pageTotal) {
+    if (end < pageTotal - 2) {
+      elements.push({ type: 'ellipsis', direction: true });
+    } else if (end == pageTotal - 2) {
+      elements.push({ type: 'num', num: pageTotal - 1 });
+    }
+    elements.push({ type: 'num', num: pageTotal });
+  }
+
+  return elements;
+}
+
 function PageNumBtn(props: PageNumBtnProps) {
   const handleClick = () => {
     props.setPos(props.num);
-
-    let elements: PaginationElement[] = [];
-    let gap: number = (defaultBtnNum - 1) / 2;
-
-    let end: number = props.num + gap > defaultBtnNum ? props.num + gap : defaultBtnNum
-    end = end < props.pageTotal ? end : props.pageTotal;
-
-    console.log('gap: ', gap);
-    console.log('end: ', end);
-
-    if (props.num > defaultBtnNum) {
-      elements.push({ type: 'num', num: 1 });
-      elements.push({ type: 'ellipsis' });
-
-      for (let i = props.num - gap; i <= end; i++) {
-        elements.push({ type: 'num', num: i });
-      }
-    } else {
-      for (let i = 1; i <= end; i++) {
-        elements.push({ type: 'num', num: i });
-      }
-    }
-
-    if (end < props.pageTotal - 1) {
-      elements.push({ type: 'ellipsis' });
-      elements.push({ type: 'num', num: props.pageTotal });
-    } else {
-      for (let i = end; i <= props.pageTotal; i++) {
-        elements.push({ type: 'num', num: props.pageTotal });
-      }
-    }
-
-    props.setElements(elements);
+    props.setElements(getElements(props.num, props.pageTotal));
   }
 
   return <Button variant='outline' size='xs' isActive={props.isActive} onClick={handleClick}>{props.num}</Button>;
@@ -58,14 +78,15 @@ interface PageEllipsisBtnProps {
   setPos: any,
   pos: number,
   direction: boolean,
-  defaultBtnNum: number,
+  setElements: any,
+  pageTotal: number,
 }
 
 function PageEllipsisBtn(props: PageEllipsisBtnProps) {
   const handleClick = () => {
-    if (props.direction) {
-      props.setPos(props.pos + props.defaultBtnNum)
-    }
+    let pos = props.direction ? props.pos + defaultBtnNum : props.pos - defaultBtnNum;
+    props.setPos(pos);
+    props.setElements(getElements(pos, props.pageTotal));
   }
 
   return <Button variant='outline' size='xs' onClick={handleClick}>...</Button>;
@@ -76,54 +97,86 @@ interface PaginationProps {
   pageSize?: number,
 }
 
-interface PaginationElement {
-  type: 'num' | 'ellipsis',
-  num?: number,
+interface DirectionBtnProps {
+  setPos: any,
+  pos: number,
+  direction: boolean,
+  setElements: any,
+  pageTotal: number,
+}
+
+function DirectionBtn(props: DirectionBtnProps) {
+  const handleClick = () => {
+    if ((props.direction && props.pos === props.pageTotal) || (!props.direction && props.pos === 1)) {
+      return;
+    }
+
+    let pos: number = props.direction ? props.pos + 1 : props.pos - 1;
+    props.setPos(pos);
+    props.setElements(getElements(pos, props.pageTotal));
+  }
+
+  return (
+    <IconButton
+      variant='outline'
+      size='xs'
+      aria-label={props.direction ? 'ChevronRight' : 'ChevronLeft'}
+      icon={props.direction ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+      onClick={handleClick}
+      isDisabled={props.direction ? props.pos === props.pageTotal : props.pos === 1}
+    />
+  );
 }
 
 export default function Pagination(props: PaginationProps) {
-  const [pos, setPos] = useState(1);
-
   const pageSize = props.pageSize ? props.pageSize : defaultPageSize;
   const pageTotal = Math.ceil(props.total / pageSize);
 
-  let initialElements: PaginationElement[] = [];
-  if (pageTotal <= defaultBtnNum + 1) {
-    for (let i = 1; i <= pageTotal; i++) {
-      initialElements.push({ type: 'num', num: i });
-    }
-  } else {
-    for (let i = 1; i <= defaultBtnNum; i++) {
-      initialElements.push({ type: 'num', num: i });
-    }
-    initialElements.push({ type: 'ellipsis' });
-    initialElements.push({ type: 'num', num: pageTotal });
-  }
-
-  const [elements, setElements] = useState(initialElements);
+  const [pos, setPos] = useState(1);
+  const [elements, setElements] = useState(getElements(1, pageTotal));
 
   const PageBtnRender = () => {
     return (
       <HStack>
         {elements.map((e, _) => {
-          return e.type == 'num' ?
+          return e.type === 'num' ?
             <PageNumBtn
-              num={e.num ? e.num : 0}
+              num={e.num}
               isActive={pos === e.num}
               setPos={setPos}
               setElements={setElements}
               pageTotal={pageTotal}
             />
             :
-            <PageEllipsisBtn direction={true} setPos={setPos} pos={pos} defaultBtnNum={defaultBtnNum} />
+            <PageEllipsisBtn
+              direction={e.direction}
+              setPos={setPos}
+              pos={pos}
+              setElements={setElements}
+              pageTotal={pageTotal}
+            />
         })}
       </HStack>
     );
   }
 
-  return (<HStack>
-    <IconButton aria-label='ChevronLeft' icon={<ChevronLeftIcon />} variant='outline' size='xs' />
-    <PageBtnRender />
-    <IconButton aria-label='ChevronRight' icon={<ChevronRightIcon />} variant='outline' size='xs' />
-  </HStack>);
+  return (
+    <HStack>
+      <DirectionBtn
+        direction={false}
+        setPos={setPos}
+        pos={pos}
+        setElements={setElements}
+        pageTotal={pageTotal}
+      />
+      <PageBtnRender />
+      <DirectionBtn
+        direction={true}
+        setPos={setPos}
+        pos={pos}
+        setElements={setElements}
+        pageTotal={pageTotal}
+      />
+    </HStack>
+  );
 }
