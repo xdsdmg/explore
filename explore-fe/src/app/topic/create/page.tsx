4eb3807b9958d83b665d617bab1be2c38196d6d7
xuedishangdemeigui <2216336843@qq.com>
2024-06-15 00:45:19 +0800

    feat(wip): devloping create topic feature

--- explore-fe/src/app/topic/create/page.tsx
+++ explore-fe/src/app/topic/create/page.tsx
@@ -3,7 +3,8 @@
 "use client"
 
 import {
-  Box, Card, Divider, Text, Button
+  Box, Card, Divider, Text, Button,
+  FormControl, InputGroup, Input, FormLabel
 }
   from '@chakra-ui/react';
 
@@ -21,20 +22,44 @@ export const codeTheme = defineStyleConfig({
 })
 
 export default function Create() {
+  let mdContent = '';
+
   const MarkdownEditor = dynamic(
     () => import("@uiw/react-markdown-editor").then((mod) => mod.default),
     { ssr: false }
   );
 
+  const handlePublish = () => {
+    console.log(mdContent);
+  };
+
   return (
     <Box className="flex flex-col items-center justify-between p-24" >
       <Card w='800px' minH='500px' variant='outline'>
-        <Box h='30px' lineHeight='30px' paddingLeft='10px'><Text as='b'>Create a new topic</Text></Box>
-        <Divider />
-        <Box padding={1}><MarkdownEditor height='460px' value="Hello Markdown!" /></Box>
+        <Box h='30px' lineHeight='30px' paddingLeft={1}><Text as='b'>Create a new topic</Text></Box>
         <Divider />
         <Box padding={1}>
-          <Button colorScheme='teal' size='sm' w='120px' variant='outline'>Publish</Button>
+          <FormControl isRequired >
+            <FormLabel htmlFor='title' fontSize={15}>Title</FormLabel>
+            <InputGroup>
+              <Input id='title' type='text' placeholder='Title' size='sm' marginBottom={1} />
+            </InputGroup>
+          </FormControl>
+          <FormControl isRequired >
+            <FormLabel htmlFor='content' fontSize={15}>Content</FormLabel>
+            <MarkdownEditor
+              id='content'
+              height='460px'
+              value='Hello Markdown!'
+              onChange={(value, viewUpdate) => {
+                mdContent = value;
+              }}
+            />
+          </FormControl>
+        </Box>
+        <Divider />
+        <Box padding={1}>
+          <Button colorScheme='teal' size='sm' w='120px' variant='outline' onClick={handlePublish}>Publish</Button>
         </Box>
       </Card>
     </Box >
