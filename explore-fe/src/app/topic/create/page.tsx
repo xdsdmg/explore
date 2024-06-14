382bfab20cbe17976ec94dedb20619a3a26c7eb8
xuedishangdemeigui <2216336843@qq.com>
2024-06-14 22:26:40 +0800

    feat(wip): devloping create topic feature

--- explore-fe/src/app/topic/create/page.tsx
+++ explore-fe/src/app/topic/create/page.tsx
@@ -1,15 +1,16 @@
-/// Code editor with highlight: https://github.com/react-simple-code-editor/react-simple-code-editor
+/// Markdown editor: https://github.com/uiwjs/react-markdown-editor
 
 "use client"
 
 import {
-  Box, Card, Divider, Text, Textarea,
-  Tabs, TabList, Tab, TabPanels, TabPanel,
-  Code,
+  Box, Card, Divider, Text, Button
 }
   from '@chakra-ui/react';
 
 import { defineStyle, defineStyleConfig } from '@chakra-ui/react'
+import dynamic from 'next/dynamic';
+import '@uiw/react-markdown-editor/markdown-editor.css';
+import '@uiw/react-markdown-preview/markdown.css';
 
 const brandPrimary = defineStyle({
   border: 'none',
@@ -19,38 +20,22 @@ export const codeTheme = defineStyleConfig({
   variants: { brandPrimary },
 })
 
-const codeBlock = `
-const GroceryItem: React.FC<GroceryItemProps> = ({ item }) => {
-  return (
-    <div>
-      <h2>{item.name}</h2>
-      <p>Price: {item.price}</p>
-      <p>Quantity: {item.quantity}</p>
-    </div>
-  );
-}
-`
-
 export default function Create() {
+  const MarkdownEditor = dynamic(
+    () => import("@uiw/react-markdown-editor").then((mod) => mod.default),
+    { ssr: false }
+  );
+
   return (
     <Box className="flex flex-col items-center justify-between p-24" >
       <Card w='800px' minH='500px' variant='outline'>
-        <Box h='40px' lineHeight='40px' paddingLeft='10px'><Text as='b'>Create a new topic</Text></Box>
+        <Box h='30px' lineHeight='30px' paddingLeft='10px'><Text as='b'>Create a new topic</Text></Box>
         <Divider />
-        <Tabs>
-          <TabList>
-            <Tab>Content</Tab>
-            <Tab>Preview</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <Code w='100%' variant='brandPrimary'>
-                <Textarea size='sm' ></Textarea>
-              </Code>
-            </TabPanel>
-            <TabPanel>World</TabPanel>
-          </TabPanels>
-        </Tabs>
+        <Box padding={1}><MarkdownEditor height='460px' value="Hello Markdown!" /></Box>
+        <Divider />
+        <Box padding={1}>
+          <Button colorScheme='teal' size='sm' w='120px' variant='outline'>Publish</Button>
+        </Box>
       </Card>
     </Box >
   );
