678bb961a18cdf597c790a55bc432d25a30413dd
zhangchi <2216336843@qq.com>
2024-11-03 11:49:43 +0800

    setup env

--- explore-be/src/main/java/com/zc/explore/controller/TopicController.java
+++ explore-be/src/main/java/com/zc/explore/controller/TopicController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,8 +30,7 @@ public class TopicController {
 
   @SuppressWarnings("rawtypes")
   @GetMapping("/list")
-  Response<ResultArray> list(
-      @RequestParam(value = "title_fuzzy_search", required = false) String titleFuzzySearch,
+  Response<ResultArray> list(@RequestParam(value = "title_fuzzy_search", required = false) String titleFuzzySearch,
       @RequestParam(value = "page", required = false) Integer page,
       @RequestParam(value = "size", required = false) Integer size) {
 
